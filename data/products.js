4331203dc49f3bf5d9f63e87c8e744806127e19d
Happpy-said <happybargaoui@gmail.com>
2025-09-22 16:37:47 +0100

    Create products.js

---
+++ data/products.js
@@ -0,0 +1,31 @@
+export const products = [
+  {
+    id: 1,
+    name: "Crème visage BIO",
+    price: 45,
+    category: "cosmetiques",
+    image: "https://via.placeholder.com/300x200?text=Crème+BIO",
+  },
+  {
+    id: 2,
+    name: "Savon naturel",
+    price: 15,
+    category: "nettoyage",
+    image: "https://via.placeholder.com/300x200?text=Savon",
+  },
+  {
+    id: 3,
+    name: "Huile essentielle",
+    price: 30,
+    category: "bio",
+    image: "https://via.placeholder.com/300x200?text=Huile+essentielle",
+  },
+  {
+    id: 4,
+    name: "Casque Bluetooth",
+    price: 120,
+    category: "hightech",
+    image: "https://via.placeholder.com/300x200?text=Casque",
+  },
+  // Ajoute autant de produits que tu veux
+]
