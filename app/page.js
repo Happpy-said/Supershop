28e4f6ab1ba0078f319499ec9dac1edb210e6885
Happpy-said <happybargaoui@gmail.com>
2025-09-22 14:36:25 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-beige text-green flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">Bienvenue sur Supershop 🛒</h1>
+      <p className="mt-4 text-lg">Votre boutique en ligne multi-catégories</p>
+    </main>
+  )
+}
 
