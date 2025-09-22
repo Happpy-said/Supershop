91454be1a2e67bc368f63384ebb7d9b907ff24bb
Happpy-said <happybargaoui@gmail.com>
2025-09-22 15:43:16 +0100

    Create page.js

---
+++ app/cosmetique/page.js
@@ -0,0 +1,10 @@
+export default function Cosmetiques() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-6 text-center">Cosmétiques</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {/* Cartes des produits cosmétiques */}
+      </div>
+    </div>
+  )
+}
