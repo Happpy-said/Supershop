2bc5568bb20b60d044f3130c6fa5bd4172250279
Happpy-said <happybargaoui@gmail.com>
2025-09-22 14:35:08 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        beige: "#f5f5dc",
+        green: "#228B22",
+      },
+    },
+  },
+  plugins: [],
+}
 
